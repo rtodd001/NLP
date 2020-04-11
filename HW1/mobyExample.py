3dbfbf6254e58f7b5ed69c59e3e86b11ead3cf5d
Sam Tapia <stapi010@ucr.edu>
2020-04-10 20:17:20 -0700

    mobyExample.py complete

--- HW1/mobyExample.py
+++ HW1/mobyExample.py
@@ -9,4 +9,11 @@ print(len(wordlist.words('mobydick.txt')))
 #number of sentences
 print(len(wordlist.sents('mobydick.txt')))
 
-print(len(re.findall("\bIshmael\b", './texts/mobydick.txt')))
+#stores a fileobject into f
+f = open("./texts/mobydick.txt")
+#stores a string into data from f
+data = f.readlines()
+#corpus is now a giant string delimited by newline character
+corpus = "\n".join(data)
+#prints the number of times Ishmael appears in the file.
+print(len(re.findall(r"\bIshmael\b", corpus)))
